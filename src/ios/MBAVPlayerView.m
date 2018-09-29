2d5cf7b3e8db7c75af39bf408d4c14f75f0ffcc4
Gavin Li <flashcode2000@qq.com>
2018-09-29 11:38:35 +0800

    resume when interrupted

--- src/ios/MBAVPlayerView.m
+++ src/ios/MBAVPlayerView.m
@@ -36,6 +36,22 @@
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(itemDidFinishPlaying:) name:AVPlayerItemDidPlayToEndTimeNotification object:[self.avPlayer currentItem]];
     
+    NSError *error = [[NSError alloc] init];
+    AVAudioSession *aSession = [AVAudioSession sharedInstance];
+    [aSession setCategory:AVAudioSessionCategoryPlayback
+              withOptions:AVAudioSessionCategoryOptionAllowBluetooth
+                    error:&error];
+    [aSession setMode:AVAudioSessionModeDefault error:&error];
+    [aSession setActive: YES error: &error];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(handleAudioSessionInterruption:)
+                                                 name:AVAudioSessionInterruptionNotification
+                                               object:aSession];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(resumePlayWhenReenterForeground)
+                                                 name:UIApplicationDidBecomeActiveNotification
+                                               object:nil];
+    
 }
 
 
@@ -50,6 +66,34 @@
         [self.avPlayer play];
     }
     
+}
+
+- (void)resumePlayWhenReenterForeground {
+    [self.avPlayer play];
+}
+
+- (void)handleAudioSessionInterruption:(NSNotification*)notification {
+    
+    NSNumber *interruptionType = [[notification userInfo] objectForKey:AVAudioSessionInterruptionTypeKey];
+    NSNumber *interruptionOption = [[notification userInfo] objectForKey:AVAudioSessionInterruptionOptionKey];
+    
+    switch (interruptionType.unsignedIntegerValue) {
+        case AVAudioSessionInterruptionTypeBegan:{
+            // • Audio has stopped, already inactive
+            // • Change state of UI, etc., to reflect non-playing state
+        } break;
+        case AVAudioSessionInterruptionTypeEnded:{
+            // • Make session active
+            // • Update user interface
+            // • AVAudioSessionInterruptionOptionShouldResume option
+            if (interruptionOption.unsignedIntegerValue == AVAudioSessionInterruptionOptionShouldResume) {
+                // Here you should continue playback.
+                [self.avPlayer play];
+            }
+        } break;
+        default:
+            break;
+    }
 }
 
 
